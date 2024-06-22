10a93358f0bf2bb8da47b92793ba75323591e7d5
Raizelminions <sebabtian.tresna@binus.ac.id>
2024-06-22 12:12:55 +0700

    UbahMargin

--- admin/inc/navigation.php
+++ admin/inc/navigation.php
@@ -2,7 +2,7 @@
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="<?php echo base_url ?>admin" class="brand-link text-sm">
-        <img src="<?php echo validate_image($_settings->info('logo'))?>" alt="Store Logo" class="brand-image img-circle elevation-3 ml-3" style="opacity: 1;width: 2.5rem;height: 2.5rem;max-height: unset">
+        <img src="<?php echo validate_image($_settings->info('logo'))?>" alt="Store Logo" class="brand-image img-circle elevation-3" style="opacity: .8;width: 2.5rem;height: 2.5rem;max-height: unset">
         <span class="brand-text font-weight-light"><?php echo $_settings->info('short_name') ?></span>
         </a>
         <!-- Sidebar -->
@@ -18,9 +18,9 @@
             <div class="os-viewport os-viewport-native-scrollbars-invisible" style="overflow-y: scroll;">
               <div class="os-content" style="padding: 0px 8px; height: 100%; width: 100%;">
                 <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-4 pb-3 mb-4 d-flex">
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                   <div class="image">
-                    <img src="<?php echo validate_image($_settings->userdata('avatar')) ?>" class="img-circle elevation-2 ml-2" alt="User Image" style="height: 2rem;object-fit: cover">
+                    <img src="<?php echo validate_image($_settings->userdata('avatar')) ?>" class="img-circle elevation-2" alt="User Image" style="height: 2rem;object-fit: cover">
                   </div>
                   <div class="info">
                     <a href="<?php echo base_url ?>admin/?page=user" class="d-block"><?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?></a>
